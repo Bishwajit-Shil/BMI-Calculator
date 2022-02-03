602254b07c2ba34f2ac456399f013d76093c541d
Bishwajit Shil <bishwasormabd23@gmail.com>
2022-02-03 21:51:25 +0600

    first one

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+
+$('.slider').EasySlides({
+      'autoplay': true, 
+      'loop': true,
+      'timeout': 3000,
+      'show': 5, 
+      'vertical': false,  
+      'reverse': false, 
+      'touchevents': true, 
+      'delayaftershow': 300, 
+      'stepbystep': true, 
+      'startslide': 0,  
+      'beforeshow': function () {},
+      'aftershow': function () {},      
+      });
+
+$('.slider_circle_10').EasySlides({
+  'show': 8 // the number of slides to show
+})
